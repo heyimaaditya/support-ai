18320ddd7ea256bf45233a800cf34fc48b5d2429
heyimaaditya <aadityatyagi158@gmail.com>
2026-02-15 12:44:22 +0530

    Ticket Service module set up

---
+++ backend-services/ticket-service/src/main/java/com/supportai/ticket/TicketServiceApplication.java
@@ -0,0 +1,15 @@
+package com.supportai.ticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.cache.annotation.EnableCaching;
+
+@SpringBootApplication
+@EnableJpaAuditing
+@EnableCaching
+public class TicketServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TicketServiceApplication.class, args);
+    }
+}
